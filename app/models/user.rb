216e5654a8d9c637b8c3392b21501856bd347840
kimino98 <k.t.ynwa0105@gmail.com>
2021-05-06 16:04:36 +0900

    本人情報確認にバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :family_name, presence: true,
+                          format: { with: /\A[ぁ-んァ-ヶ一-龥]/ }
+  validates :first_name, presence: true,
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥]/ }
+  validates :family_name_kana, presence: true,
+                               format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ }
+  validates :first_name_kana, presence: true,
+                              format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ }
   validates :birth_day, presence: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
