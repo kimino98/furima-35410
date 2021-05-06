483b8240fdec6cf209ff0413b7c5fbf814de0765
kimino98 <k.t.ynwa0105@gmail.com>
2021-05-06 17:49:28 +0900

    エラーハンドリングの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,14 +14,4 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true,
                               format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ }
   validates :birth_day, presence: true
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, 
-                    uniqueness: true,
-                    format:{ with: VALID_EMAIL_REGEX }
-
-  validates :password, length: { minimum: 6 },
-                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i},
-                       confirmation: true
-
 end
