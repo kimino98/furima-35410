6ca6a12e9c63bca7150db1efb3292f314dee2e3b
kimino98 <k.t.ynwa0105@gmail.com>
2021-05-09 11:28:39 +0900

    ユーザー管理機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,17 @@ class User < ApplicationRecord
 
     with_options presence: true do
       validates :nickname
-      validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/ }
-      validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/ }
-      validates :family_name_kana, format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ }
-      validates :first_name_kana, format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ }
       validates :birth_day
+
+      with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]/ } do
+       validates :family_name 
+       validates :first_name
+      end
+
+      with_options format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ } do
+       validates :family_name_kana
+       validates :first_name_kana
+      end
     end
       validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
   end
