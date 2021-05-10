f578633e7a5f06250eb45b8c34bf52ebf2adcbc0
kimino98 <k.t.ynwa0105@gmail.com>
2021-05-10 19:00:52 +0900

    商品出品ページ表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '1234'
+    end
   end
 
   def configure_permitted_parameters
