3f840826c5dbeac67a6248c4d2b452049908a74e
kimino98 <k.t.ynwa0105@gmail.com>
2021-05-14 15:35:07 +0900

    buyers(購入者モデル)バリデーション記述

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,4 +1,14 @@
 class Buyer < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :region
 
-  
+  belongs_to :user
+
+  with_options presence: true do
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}\z/ }
+    validates :region_id, numericality: { other_than: 1 }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
+  end
 end
