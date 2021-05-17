85a62da49cd3f80a4d4849d6d540af5df0550fbd
kimino98 <k.t.ynwa0105@gmail.com>
2021-05-17 19:01:39 +0900

    ユーザー情報の条件分岐

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -1,16 +1,21 @@
 class PurchaseHistoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_purchase_history, only: [:index, :create]
+  before_action :sold_out, only: [:index]
 
   def index
     @buyer_purchase_history = BuyerPurchaseHistory.new
-    @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @buyer_purchase_history = BuyerPurchaseHistory.new(buyer_purchase_history_params)
-    if @buyer_purchase_history.save
+    if @buyer_purchase_history.valid? 
+      @buyer_purchase_history.save
       redirect_to root_path
-    else
+     else
       render :index
     end
   end
@@ -19,6 +24,13 @@ class PurchaseHistoriesController < ApplicationController
   def buyer_purchase_history_params
     params.require(:buyer_purchase_history).permit(:post_code, :region_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
-end
 
+  def set_purchase_history
+    @item = Item.find(params[:item_id])
+  end
+  
+  def sold_out
+    redirect_to root_path if @item.purchase_history.present?
+  end
+end
 
