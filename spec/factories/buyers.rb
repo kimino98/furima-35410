bdc38796c471c274b4e8791b5449c8445c5fc5c4
kimino98 <k.t.ynwa0105@gmail.com>
2021-05-18 11:16:23 +0900

    buyerテストコード記述

--- spec/factories/buyers.rb
+++ spec/factories/buyers.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :buyer do
-    
+    post_code           {123-4567}
+    region_id           {2}
+    city                {'東京'}
+    address             {'1-1-1'}
+    building_name       {'東京ハイツ'}
+    phone_number        {'09012345678'}
+    purchase_history_id {'2'}
   end
 end
--- spec/models/buyer_spec.rb
+++ spec/models/buyer_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Buyer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '購入者情報保存' do
+    before do
+      @buyer = FactoryBot.build(:buyer)
+    end
+
+    context '内容に問題がない場合' do
+      it '全ての情報が正しく入力されているとき' do
+        expect(@buyer).to be_valid
+      end
+    end
+
+    context '内容に問題がある時' do
+      it 'post_codeが空だと保存されない' do
+        @buyer.post_code = ''
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include("Post code can't be blank")
+      end
+
+      it 'post_codeにハイフンがないと保存できない' do
+        
+      end
+
+      it 'region_idが空だと保存できない' do
+        
+      end
+
+      it 'cityが空だと保存できない' do
+        
+      end
+
+      it 'addressが空だと保存できない' do
+        
+      end
+
+      it 'phone_numberが空だと保存できない' do
+        
+      end
+
+      it 'phone_numberが11桁以上だと保存できない' do
+        
+      end
+    end
+  end
 end
