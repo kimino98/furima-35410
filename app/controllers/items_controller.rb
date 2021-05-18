0b73e15e2fb6e66963ca1aa20ee4b77104be2137
kimino98 <k.t.ynwa0105@gmail.com>
2021-05-18 19:02:30 +0900

    editコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :sold_out, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -53,5 +54,9 @@ class ItemsController < ApplicationController
 
   def contributor_confirmation
     redirect_to root_path unless @item.user == current_user
+  end
+
+  def sold_out
+    redirect_to root_path if @item.purchase_history.present?
   end
 end
